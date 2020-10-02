a934c5fe23f042c71f65301d46969a950f544cc0
Jeonghyuk <calseis@gmail.com>
2020-10-02 15:22:02 +0900

    Updates

---
+++ precache-manifest.447775e958a5c44d1174e54fb3ddf2fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16742a5308eb8d47d2247cfbe44d3a7f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "342c54e19076e5d23c1a",
+    "url": "/movie_app_2019/static/css/main.b22a6a6b.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "342c54e19076e5d23c1a",
+    "url": "/movie_app_2019/static/js/main.06211a42.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
